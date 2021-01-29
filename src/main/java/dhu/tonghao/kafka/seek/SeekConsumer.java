617ac91045c5eb713916bf17a63ff0bf9a76a3a7
DHU-th <1434672306@qq.com>
2021-01-29 14:08:51 +0800

    根据å®时间设置消费位移

--- src/main/java/dhu/tonghao/kafka/seek/SeekConsumer.java
+++ src/main/java/dhu/tonghao/kafka/seek/SeekConsumer.java
@@ -1,20 +1,18 @@
 package dhu.tonghao.kafka.seek;
 
 import java.time.Duration;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Properties;
-import java.util.Set;
+import java.util.*;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndTimestamp;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 public class SeekConsumer {
-    public static final String brokerList = "47.93.121.123:9092";
-    public static final String topic = "kafka_demo_analysis";
+    public static final String brokerList = "47.93.121.123:9092，47.93.121.123:9093，47.93.121.123:9094";
+    public static final String topic = "kafka-seek-analysis";
     /** 消费组的名称 */
     public static final String groupId = "kafka-learner";
 
@@ -35,16 +33,22 @@ public class SeekConsumer {
         /**
          * 注意,此时消费者只是订阅了主题,但是还没有被分配到指定的分区 直接指定会报异常
          **/
+        // 为每个分区设置的offset，需要根据业务来
         //consumer.seek(new TopicPartition(topic, 100), 10);
         Set<TopicPartition> assignment = new HashSet<>();
         while (assignment.isEmpty()) {
             consumer.poll(Duration.ofMillis(1000));
-            assignment = consumer.assignment();
+            assignment = consumer.assignment();//获取订阅的分区
         }
+        long fetchDateTime = new Date().getTime() - 2 * 60 * 60 * 1000;
+        Map<TopicPartition, Long> timestampMap = new HashMap<>();
         /** 确保被分配了指定的分区,再用seek指定这些分区开始位移消费的位置 */
         for (TopicPartition tp : assignment) {
-            consumer.seek(tp, 10);
+            // 比如说要从2个小时前的消息开始消费
+            timestampMap.put(tp, fetchDateTime);
         }
+        Map<TopicPartition, OffsetAndTimestamp> offsets = consumer.offsetsForTimes(timestampMap);
+        offsets.forEach((k, v) -> consumer.seek(k, v.offset()));
         while (true) {
             ConsumerRecords<String, String> poll = consumer.poll(Duration.ofMillis(1000));
             //TODO
